a51729155f9807f5591876062694e79d3707ffec
Nathan LaFreniere <quitlahok@gmail.com>
2015-11-14 16:44:28 -0800

    update lab and code, fix linting

--- test/index.js
+++ test/index.js
@@ -3,11 +3,11 @@
 const Events = require('..');
 const Wadofgum = require('wadofgum');
 
-let lab = exports.lab = require('lab').script();
-let expect = require('code').expect;
-let it = lab.test;
+const lab = exports.lab = require('lab').script();
+const expect = require('code').expect;
+const it = lab.test;
 
-it('can extend the base model', function (done) {
+it('can extend the base model', (done) => {
 
     class User extends Wadofgum.mixin(Events) {};
     expect(User.on).to.be.a.function();
@@ -15,54 +15,54 @@ it('can extend the base model', function (done) {
     done();
 });
 
-it('fires the create event when instantiating a model', function (done) {
+it('fires the create event when instantiating a model', (done) => {
 
     class User extends Wadofgum.mixin(Events) {};
-    User.on('create', function (model) {
+    User.on('create', (model) => {
 
         expect(model.name).to.equal('test');
         expect(model.age).to.equal(20);
         done();
     });
 
-    let user = new User({ name: 'test', age: 20 });
+    const user = new User({ name: 'test', age: 20 });
     expect(user).to.contain('name', 'age');
 });
 
-it('can create an emitter when emitting', function (done) {
+it('can create an emitter when emitting', (done) => {
 
     class User extends Wadofgum.mixin(Events) {};
     expect(User.meta.has('emitter')).to.equal(false);
-    User.emit('test', 'data').then(function () {
+    User.emit('test', 'data').then(() => {
 
         expect(User.meta.has('emitter')).to.equal(true);
         done();
     });
 });
 
-it('can create an emitter when adding a listener', function (done) {
+it('can create an emitter when adding a listener', (done) => {
 
     class User extends Wadofgum.mixin(Events) {};
     expect(User.meta.has('emitter')).to.equal(false);
-    User.on('test', function () { });
+    User.on('test', () => {});
     expect(User.meta.has('emitter')).to.equal(true);
     done();
 });
 
-it('can emit an event', function (done) {
+it('can emit an event', (done) => {
 
     class User extends Wadofgum.mixin(Events) {};
-    User.on('test', function (data) {
+    User.on('test', (data) => {
 
         expect(data).to.equal('data');
     });
 
-    User.on('test2', function (data) {
+    User.on('test2', (data) => {
 
         done();
     });
 
-    User.emit('test', 'data').then(function () {
+    User.emit('test', 'data').then(() => {
 
         User.emit('test2', 'data');
     });
